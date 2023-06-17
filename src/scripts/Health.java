21bd414208fe78c9697b2fc324886850d85e2f07
Mohamedsalem80 <mohamed3salem1@gmail.com>
2023-06-17 19:13:50 +0300

    11th commit

--- src/scripts/Health.java
+++ src/scripts/Health.java
@@ -6,17 +6,27 @@ import System.Collections.Generic;
 import System.ComponentModel.Design.Serialization;
 import Unity.Engine;
 import Unity.TMPro;
+import Unity.UI;
 
 public class Health extends MonoBehaviour {
 
+    public Image barImage;
     public double health = 100;
     public TextMeshProUGUI heal;
-     void Update()
+
+    void Update()
     {
-        if(health<100 && health>0)
+        if (health < 100 && health > 0)
         {
             health += 0.05;
         }
         heal.setText(String.valueOf((int)health));
+        UpdateHealthbar();
+    }
+
+    void UpdateHealthbar()
+    {
+        barImage.fillAmount = (float)(health / 100);
     }
 }
+
